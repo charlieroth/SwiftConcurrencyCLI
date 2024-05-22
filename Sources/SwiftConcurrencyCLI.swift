ce82ee5dbeab9e8a15f9417bb3497f1a1eab0b3d
charlieroth <charlieroth4@gmail.com>
2024-05-22 23:00:31 +0200

    create new application entrypoint

---
+++ Sources/SwiftConcurrencyCLI.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftConcurrencyCLI.swift
+//
+//
+//  Created by Charles Roth on 2024-05-22.
+//
+
+import Foundation
+
+@main
+struct SwiftConcurrencyCLI {
+    static func main() {
+        
+    }
+}
